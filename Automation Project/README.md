d5cac6abf877c47f70d63b02c17ddb410dc5453d
VajpeyeeAmit <98537120+VajpeyeeAmit@users.noreply.github.com>
2022-11-30 23:14:15 +0000

    Analysis of Data on Indian Railways
    Link to dataset: https://www.kaggle.com/datasets/sripaadsrinivasan/indian-railways-dataset
    Data collected by the uploader and his team. There are 3 data files in JSON Format.

---
+++ Automation Project/README.md
@@ -0,0 +1,3 @@
+# DAP_Project_New
+
+Testing file
---
+++ Automation Project/mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+        {
+            user: "ravjyotduggal",
+            pwd: "rav1233",
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "dap"
+                }
+            ]
+        }
+);
